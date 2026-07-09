4d6d93d23f5b550fe67159094455a0ff1850da97
Efimovs Agency <efimovs.agency@email.com>
2026-07-09 16:02:19 +0300

    fix

--- views/home.php
+++ views/home.php
@@ -8,11 +8,10 @@
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
     
-    <?= Vite::assets([
-        'src/css/style.css',
-        'src/css/responsive.css',
-        'src/js/script.js'
-    ]) ?>
+
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/responsive.css">
+    <script src="/js/script.js" defer></script>
 
     <style>
         html { scrollbar-gutter: stable; }
